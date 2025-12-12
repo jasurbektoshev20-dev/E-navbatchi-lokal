d9c31a88808de0f08757a6317eda0efd5402c6ab
Sirojiddin <sirojiddin.devs@gmail.com>
2025-12-12 10:40:26 +0500

    fixed

--- token_proxy.php
+++ token_proxy.php
@@ -7,28 +7,39 @@ $input = json_decode(file_get_contents('php://input'), true);
 $contractId = isset($input['contractId']) ? intval($input['contractId']) : 0;
 
 if (!$contractId) {
-    echo json_encode(['success'=>false, 'msg'=>'No contractId']);
+    echo json_encode(['success' => false, 'msg' => 'No contractId']);
     exit;
 }
 
-// credentials should be stored in server config / env, not in code
-$username = 'mg_api@uzgps.uz';
-$password = 'hc_b1c5vW%';
-$payload = json_encode([
-    'username' => $username,
-    'password' => $password,
-    'contractId' => $contractId,
-    'show-objects-of-all-contracts' => true
-]);
+$curl = curl_init();
 
-$ch = curl_init('https://smpo.uzgps.uz/api/auth/login');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-$res = curl_exec($ch);
-$http = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://smpo.uzgps.uz/api/auth/login',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => '{
+        "username": "mg_api@uzgps.uz",
+        "password": "hc_b1c5v%W",
+        "contractId": ' . $contractId . ',
+        "show-objects-of-all-contracts": true
+    }',
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json',
+        'Accept: application/json',
+    ),
+));
+
+$response = curl_exec($curl);
+
+// echo '<pre>';
+// print_r($payload);
+// echo '</pre>';
+// die();
 
 http_response_code($http);
-echo $res;
+echo $response;
