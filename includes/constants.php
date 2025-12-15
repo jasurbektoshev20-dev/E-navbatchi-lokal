97b45ce2782043ba432cd1f7dfdd84cc60d89db1
Jasurbek Toshev <jasurbektoshev20@gmail.com>
2025-12-15 09:37:33 +0500

    fix

--- includes/constants.php
+++ includes/constants.php
@@ -3,10 +3,10 @@
 /****************************************************************
  *                            	 constants.php			 		*
  *                          -------------------			  		*
- *     begin                : 01.03.2015 y						*
- *     copyright            : Sayqal Solutions 2015				*
- *     email                : info@sayqal.uz						*
- *	    Written by		   		 : Botirjon G Olimov			*
+ *     begin                : 01.10.2025 y						*
+ *     copyright            : by Jasurbek and Javokhir				*
+ *     email                : jasurbektoshev20@gmail.com						*
+ *	   Written by		    : J.N Toshev			*
  ****************************************************************/
 
 if (!defined('ARM_IN')) {
